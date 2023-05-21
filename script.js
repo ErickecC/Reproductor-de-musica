edf30f1694c16491633e335f6d627f9ff6f0b734
DreamArtsltd <129808364+DreamArtsltd@users.noreply.github.com>
2023-05-21 16:38:14 -0600

    Update script.js

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ let now_playing = document.querySelector('.now-playing');
   
  const music_list = [ 
      { 
-         img : 'Imágenes/IMG-9899.webp', 
+         img : 'Imágenes/IMG_9902.jpeg', 
          name : 'NI BIEN NI MAL', 
          artist : 'Bad Bunny', 
          music : 'Musica/NI BIEN NI MAL.mp3' 
